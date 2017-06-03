5be9d13bca3b028fa03ce36f9e9ac86bc5beb5b8
jmuto2 <jmuto2@gmail.com>
2017-06-02 21:15:07 -0400

    add table with button to show players info

--- app/Http/Controllers/PlayersController.php
+++ app/Http/Controllers/PlayersController.php
@@ -2,25 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class PlayersController extends Controller {
 
-  public function index()
+  public function index(Request $request)
   {
-    $players = [
-      ['id' => 1, 'first_name' => 'Joe', 'last_name' => 'Muto'],
-      ['id' => 2, 'first_name' => 'Pete', 'last_name' => 'Smith'],
-      ['id' => 3, 'first_name' => 'Larry', 'last_name' => 'Fitzgerald'],
-      ['id' => 4, 'first_name' => 'Scott', 'last_name' => 'Grates'],
-      ['id' => 5, 'first_name' => 'Mike', 'last_name' => 'Young'],
-      ['id' => 6, 'first_name' => 'Chris', 'last_name' => 'Muto'],
-      ['id' => 7, 'first_name' => 'Tony', 'last_name' => 'Barkett'],
-      ['id' => 8, 'first_name' => 'Rick', 'last_name' => 'Bernier'],
-      ['id' => 9, 'first_name' => 'Kevin', 'last_name' => 'Knipe'],
-      ['id' => 10, 'first_name' => 'Matt', 'last_name' => 'Mitchell'],
-    ];
-
-    return view('players.index', ['players' => $players]);
+    return view('players.index');
   }
 }
